c489b9afbc48aa936a2ea94ba46054b166ef095d
josephblair92 <josephblair92@gmail.com>
2026-08-14 22:37:58 -0400

    Game structure

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,41 @@
 import { useState } from 'react'
 import { Case } from './ui/Case'
-import { shuffleCases } from './utils'
+import { formatterUSD, shuffleCases } from './utils'
 import type { CaseProps } from './types'
 import { DollarAmounts } from './ui/DollarAmounts'
+
+const casesPerRound = [
+  ...[...Array(7).keys()].slice(1).reverse(),
+  ...Array(3).fill(1)
+]
 
 function App() {
 
   const [cases, setCases] = useState<Array<CaseProps>>(shuffleCases())
   const [userCaseSelected, setUserCaseSelected] = useState(false)
+  const [round, setRound] = useState(0)
+  const [casesLeftInRound, setCasesLeftInRound] = useState(0)
+  const [gameOver, setGameOver] = useState(false)
+
+  const advanceRound = () => {
+    if (round === casesPerRound.length) {
+      setGameOver(true)
+    } else {
+      setCasesLeftInRound(casesPerRound[round])
+      setRound(round => round + 1)
+    }
+  }
+
+  const calculateDealAmount = (roundNum: number) => {
+    const unrevealedAmounts = cases
+      .filter((curCase) => !curCase.opened)
+      .map((curCase) => curCase.amount)
+    const avgAmount =
+      unrevealedAmounts.reduce((sum, cur) => sum + cur, 0) /
+      unrevealedAmounts.length
+    const roundMultiplier = roundNum / 10
+    return Math.floor(avgAmount * roundMultiplier)
+  }
 
   const selectUserCase = (caseNum: number) => {
     setCases(cases => {
@@ -20,6 +48,7 @@ function App() {
       return cases
     })
     setUserCaseSelected(true)
+    advanceRound()
   }
 
   const openCase = (caseNum: number) => {
@@ -32,15 +61,43 @@ function App() {
       ]
       return cases
     })
+    setCasesLeftInRound(casesLeftInRound => casesLeftInRound - 1)
+  }
+
+  const acceptDeal = () => {
+    setGameOver(true)
+  }
+
+  const rejectDeal = () => {
+    advanceRound()
+  }
+
+  let gameStatus
+  if (gameOver) {
+    gameStatus = <p className='text-2xl'>Game over.</p>
+  }
+  else if (!userCaseSelected) {
+    gameStatus = <p className='text-2xl'>Select your case.</p>
+  } else if (casesLeftInRound === 0) {
+    gameStatus = <div className='flex flex-col gap-2'>
+      <p className='text-2xl'>Deal or No Deal?</p>
+      <p>Offer: <b>{formatterUSD.format(calculateDealAmount(round))}</b></p>
+      <div className='flex gap-3'>
+        <button className='bg-green-400 rounded-md p-3 min-w-32' onClick={acceptDeal}>Deal</button>
+        <button className='bg-red-400 rounded-md p-3 min-w-32' onClick={rejectDeal}>No Deal</button>
+      </div>
+    </div>
+  } else {
+    gameStatus = <div><p className='text-2xl'>Open a case.</p><p>You have <b>{casesLeftInRound}</b> left to open before the next offer.</p></div>
   }
 
   return (
     <div className='flex h-screen'>
       <div className='flex flex-wrap gap-3 w-1/2 h-fit p-3'>
-        {cases.map((curCase: CaseProps) => <Case {...curCase} enabled={!userCaseSelected || (!curCase.opened && !curCase.userCase)} onClick={!userCaseSelected ? selectUserCase : openCase} key={curCase.id} />)}
+        {cases.map((curCase: CaseProps) => <Case {...curCase} enabled={!userCaseSelected || (casesLeftInRound > 0 && !curCase.opened && !curCase.userCase)} showAmount={curCase.opened || gameOver} onClick={!userCaseSelected ? selectUserCase : openCase} key={curCase.id} />)}
       </div>
       <div className='flex flex-col w-1/2 h-full p-2 justify-between'>
-        <p className='text-2xl'>{userCaseSelected ? 'Open a case.' : 'Select your case.'}</p>
+        {gameStatus}
         <DollarAmounts cases={cases} />
       </div>
     </div>
--- ui/src/ui/Case.tsx
+++ ui/src/ui/Case.tsx
@@ -3,13 +3,14 @@ import { formatterUSD } from "../utils"
 
 export interface CaseUIProps extends CaseProps {
     enabled: boolean,
+    showAmount: boolean,
     onClick: (num: number) => void
 }
 
-export const Case = ({ id, amount, opened, userCase, enabled, onClick }: CaseUIProps) => {
+export const Case = ({ id, amount, opened, userCase, enabled, showAmount, onClick }: CaseUIProps) => {
     const bgColor = userCase ? 'bg-yellow-400' : opened ? 'bg-red-400' : 'bg-green-400'
     return <button className={`flex flex-col justify-center items-center min-w-32 h-24 ${bgColor} rounded-xl`} disabled={!enabled} onClick={() => onClick(id)}>
         <span className='font-bold text-2xl'>{id}</span>
-        <span>{opened ? formatterUSD.format(amount) : '?'}</span>
+        <span>{showAmount ? formatterUSD.format(amount) : '?'}</span>
     </button>
 }
