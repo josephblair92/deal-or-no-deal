e6d7ffa173a63796435409351d3e633b2c011333
josephblair92 <josephblair92@gmail.com>
2026-08-14 20:15:56 -0400

    Only reveal dollar amount for opened cases

--- ui/src/ui/Case.tsx
+++ ui/src/ui/Case.tsx
@@ -10,6 +10,6 @@ export const Case = ({ id, amount, opened, userCase, enabled, onClick }: CaseUIP
     const bgColor = userCase ? 'bg-yellow-400' : opened ? 'bg-red-400' : 'bg-green-400'
     return <button className={`flex flex-col justify-center items-center min-w-32 h-24 ${bgColor} rounded-xl`} disabled={!enabled} onClick={() => onClick(id)}>
         <span className='font-bold text-2xl'>{id}</span>
-        <span>{formatterUSD.format(amount)}</span>
+        <span>{opened ? formatterUSD.format(amount) : '?'}</span>
     </button>
 }
